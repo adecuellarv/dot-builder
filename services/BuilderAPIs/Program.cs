f543578e24a78f18f5987312555d7a054b7c8195
Ade Cuellar <76180286+adecuellarv@users.noreply.github.com>
2024-10-12 01:32:51 -0600

    Update Program.cs

--- services/BuilderAPIs/Program.cs
+++ services/BuilderAPIs/Program.cs
@@ -21,9 +21,45 @@ builder.Services.AddScoped<IItemsRepository, ItemsRepository>();
 builder.Services.AddScoped<ICategoriesRepository, CategoriesRepository>();
 builder.Services.AddScoped<IComponentsRepository, ComponentsRepository>();
 
+// Configuracion de servicios
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin() // Permitir cualquier origen
+               .AllowAnyMethod() // Permitir cualquier metodo (GET, POST, etc.)
+               .AllowAnyHeader(); // Permitir cualquier encabezado
+    });
+});
+
 var app = builder.Build();
 
 // Configurar el middleware
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Configure the HTTP request pipeline.
+app.UseCors("AllowAllOrigins");
+
+// Configure the HTTP request pipeline.
 app.UseAuthorization();
+
 app.MapControllers();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
